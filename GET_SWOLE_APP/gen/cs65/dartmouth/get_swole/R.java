7ee4e41012b124df1dfa4c6556bace6d7722114a
Cameron J. Price <Cameron J. Price@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-13 22:07:55 +0000

    added the Main Fragment skeleton

--- GET_SWOLE_APP/gen/cs65/dartmouth/get_swole/R.java
+++ GET_SWOLE_APP/gen/cs65/dartmouth/get_swole/R.java
@@ -265,8 +265,9 @@ containing a value of this type.
         public static final int activity_main=0x7f030000;
         public static final int activity_profile=0x7f030001;
         public static final int fragment_main_dummy=0x7f030002;
-        public static final int fragment_schedule=0x7f030003;
-        public static final int history_tab_fragment=0x7f030004;
+        public static final int fragment_progress_main=0x7f030003;
+        public static final int fragment_schedule=0x7f030004;
+        public static final int fragment_workout=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f0b0000;
