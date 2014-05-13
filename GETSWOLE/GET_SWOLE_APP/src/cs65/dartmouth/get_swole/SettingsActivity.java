32949e30ecc2039c00928622488d71a6df83c72b
Cameron J. Price <Cameron J. Price@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-13 22:11:26 +0000

    Minor updates

--- GETSWOLE/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/SettingsActivity.java
+++ GETSWOLE/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/SettingsActivity.java
@@ -18,6 +18,7 @@ import android.preference.PreferenceActivity;
 public class SettingsActivity extends PreferenceActivity {
 
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
--- GETSWOLE/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/WorkoutFragment.java
+++ GETSWOLE/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/WorkoutFragment.java
@@ -1,17 +1,11 @@
 package cs65.dartmouth.get_swole;
 
-import java.util.ArrayList;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 public class WorkoutFragment extends ListFragment {
 	
