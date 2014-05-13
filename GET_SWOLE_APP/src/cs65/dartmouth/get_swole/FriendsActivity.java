a9a2e3047d67f4fb0a7b7080fd049e4f23161c20
Cameron J. Price <Cameron J. Price@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-13 22:25:37 +0000

    Adding more skeleton code

---
+++ GET_SWOLE_APP/src/cs65/dartmouth/get_swole/FriendsActivity.java
@@ -0,0 +1,20 @@
+package cs65.dartmouth.get_swole;
+
+import cs65.dartmouth.get_swole.R;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class FriendsActivity extends Activity {
+	
+	/**
+	 * onCreate()
+	 * @param Bundle savedInstanceStates
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_friends);
+		
+	}
+	
+}
