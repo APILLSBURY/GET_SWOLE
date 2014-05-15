07118d7de8fcfc04ca34930f94f801d4df348bb1
Andrew D. Pillsbury <Andrew D. Pillsbury@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-15 21:22:41 +0000

    added package for classes and database

---
+++ GETSWOLE/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/classes/Frequency.java
@@ -0,0 +1,44 @@
+package cs65.dartmouth.get_swole.classes;
+
+import java.util.Calendar;
+
+public class Frequency {
+
+	private int day;
+	private Calendar startDate;
+	private Calendar endDate;
+	
+	public Frequency (int day) {
+		this.day = day;
+		startDate = null;
+		endDate = null;
+	}
+	
+	
+	//GETTER METHODS
+	public int getDay() {
+		return day;
+	}
+	
+	public Calendar getStartDate() {
+		return startDate;
+	}
+	
+	public Calendar getEndDate() {
+		return endDate;
+	}
+	
+	
+	//SETTER METHODS
+	public void setDay(int d) {
+		day = d;
+	}
+	
+	public void setStartDate(Calendar sd) {
+		startDate = sd;
+	}
+	
+	public void setEndDate(Calendar ed) {
+		endDate = ed;
+	}
+}
