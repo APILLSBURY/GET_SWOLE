7d5d80c3cdd2c218407ae8012fef2e09830fc539
Andrew D. Pillsbury <Andrew D. Pillsbury@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-15 22:31:51 +0000

    finished the helper

--- GETSWOLE/Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/database/DatabaseHelper.java
+++ GETSWOLE/Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/database/DatabaseHelper.java
@@ -7,6 +7,7 @@ import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+	//Workout table
 	public static final String TABLE_NAME_WORKOUT = "workout";
 	public static final String WORKOUT_ID = "_id";
 	public static final String WORKOUT_NAME = "name";
@@ -15,12 +16,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public static final String WORKOUT_START_DATE = "startdate";
 	public static final String WORKOUT_FREQUENCY_LIST = "frequencylist";
 	
-	
+	//Workout instance table
 	public static final String TABLE_NAME_WORKOUT_INSTANCE = "workout_instance";
-	public static final String WORKOUT_INSTANCES_NAME = "name";
-	public static final String WORKOUT_INSTANCES_EXERCISE_LIST = "exerciselist";
-	public static final String WORKOUT_INSTANCES_TIME = "time";
+	public static final String WORKOUT_INSTANCE_ID = "_id";
+	public static final String WORKOUT_INSTANCE_NAME = "name";
+	public static final String WORKOUT_INSTANCE_EXERCISE_LIST = "exerciselist";
+	public static final String WORKOUT_INSTANCE_TIME = "time";
 	
+	//Exercise table
 	public static final String TABLE_NAME_EXERCISE = "exercise";
 	public static final String EXERCISE_ID = "_id";
 	public static final String EXERCISE_NAME = "name";
@@ -29,6 +32,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public static final String EXERCISE_REPS_GOAL = "repsgoal";
 	public static final String EXERCISE_WEIGHT_GOAL = "weightgoal";
 	
+	//Frequency table
 	public static final String TABLE_NAME_FREQUENCY = "frequency";
 	public static final String FREQUENCY_ID = "_id";
 	public static final String FREQUENCY_DAY = "day";
@@ -50,9 +54,39 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             + WORKOUT_START_DATE + " DATE, "
             + WORKOUT_FREQUENCY_LIST + " BLOB, " + ");";  
     
+    public static final String CREATE_TABLE_EXERCISE = "CREATE TABLE IF NOT EXISTS "
+            + TABLE_NAME_WORKOUT_INSTANCE + " ("
+            + WORKOUT_INSTANCE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + WORKOUT_INSTANCE_NAME + " TEXT, "
+            + WORKOUT_EXERCISE_LIST + " BLOB, "
+            + WORKOUT_INSTANCE_TIME + " DATETIME, " + ");";  
+    
+    public static final String CREATE_TABLE_WORKOUT_INSTANCE = "CREATE TABLE IF NOT EXISTS "
+            + TABLE_NAME_EXERCISE + " ("
+            + EXERCISE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + EXERCISE_NAME + " TEXT, "
+            + EXERCISE_REPS + " INT, "
+            + EXERCISE_WEIGHT + " INT, "
+            + EXERCISE_REPS_GOAL + " INT, "
+            + EXERCISE_WEIGHT_GOAL + " INT, " + ");";  
+    
+    public static final String CREATE_TABLE_FREQUENCY = "CREATE TABLE IF NOT EXISTS "
+            + TABLE_NAME_FREQUENCY + " ("
+            + FREQUENCY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + FREQUENCY_DAY + " INT, "
+            + FREQUENCY_START_DATE + " DATE, "
+            + FREQUENCY_END_DATE + " DATE, " + ");";
+    
+    public static final String[] TABLE_NAMES = {TABLE_NAME_WORKOUT, TABLE_NAME_WORKOUT_INSTANCE, 
+    												TABLE_NAME_EXERCISE, TABLE_NAME_FREQUENCY};
+    public static final String[] CREATE_TABLE_COMMANDS = {CREATE_TABLE_WORKOUT, CREATE_TABLE_WORKOUT_INSTANCE, 
+		CREATE_TABLE_EXERCISE, CREATE_TABLE_FREQUENCY};
+    
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(CREATE_TABLE_ENTRIES);
+    	for (String createCommand : CREATE_TABLE_COMMANDS) {
+	        database.execSQL(createCommand);
+    	}
     }
 
     @Override
@@ -60,7 +94,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         Log.w(DatabaseHelper.class.getName(), "Upgrading database from version "
             + oldVersion + " to " + newVersion
             + ", which will destroy all old data");
-        database.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_ENTRIES);
+
+    	for (String tableName : TABLE_NAMES) {
+            database.execSQL("DROP TABLE IF EXISTS " + tableName);
+    	}
         onCreate(database);
     }
 
