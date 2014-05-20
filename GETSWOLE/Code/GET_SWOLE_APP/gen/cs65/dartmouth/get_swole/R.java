343765c1eb485779526d28c9e799db7f964e1427
Cameron J. Price <Cameron J. Price@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-20 18:44:38 +0000

    update strings

--- GETSWOLE/Code/GET_SWOLE_APP/gen/cs65/dartmouth/get_swole/R.java
+++ GETSWOLE/Code/GET_SWOLE_APP/gen/cs65/dartmouth/get_swole/R.java
@@ -455,7 +455,7 @@ containing a value of this type.
         public static final int profile_weight=0x7f07003e;
         public static final int save_profile_message=0x7f070048;
         /**  GCM Strings 
- <string name="server_url">http://getswoleandroidapp.appspot.com</string> 
+<string name="server_url">http://getswoleandroidapp.appspot.com</string>
          */
         public static final int server_url=0x7f07004a;
         public static final int settings_title=0x7f07001e;
