f64f8674c8c509e30eaecf75ff28671469c0ca6d
Andrew D. Pillsbury <Andrew D. Pillsbury@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-21 05:11:27 +0000

    committed checkpoint

---
+++ Checkpoint/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/WorkoutDoActivity.java
@@ -0,0 +1,116 @@
+package cs65.dartmouth.get_swole;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import cs65.dartmouth.get_swole.classes.Exercise;
+import cs65.dartmouth.get_swole.classes.Workout;
+import cs65.dartmouth.get_swole.classes.WorkoutInstance;
+import cs65.dartmouth.get_swole.database.DatabaseWrapper;
+
+public class WorkoutDoActivity extends Activity {
+	
+	WorkoutInstance workoutInstance;
+	ExerciseArrayAdapter mAdapter;
+
+	/**
+	 * onCreate()
+	 * @param Bundle savedInstanceStates
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_workout);	
+		
+		// Display		
+		TextView nameView = (TextView) findViewById(R.id.workoutName);
+				
+		// Set the buttons to have the correct names and callbacks
+		Button button1 = (Button) findViewById(R.id.button1);
+		Button button2 = (Button) findViewById(R.id.button2);
+		
+		// set the button text/callbacks
+		button1.setText(getString(R.string.timer));
+		button1.setOnClickListener(new OnClickListener() {
+		     @Override
+		     public void onClick(View v) {
+			        // begin timer
+		    	 	/*
+			    	DialogFragment fragment = AppDialogFragment.newInstance(AppDialogFragment.DIALOG_ID_TIMER);
+	     	        fragment.show(getFragmentManager(), getString(R.string.dialog_fragment_tag_timer)); */
+		     }
+		});
+		button2.setText(getString(R.string.stop_workout_button));			
+		button2.setOnClickListener(new OnClickListener() {
+		     @Override
+		     public void onClick(View v) {
+		           onFinishWorkout();
+		     }
+		});
+					
+		// Initialize all instance variables
+		workoutInstance = new WorkoutInstance();
+		workoutInstance.setTime(Calendar.getInstance());
+		
+		long id = getIntent().getExtras().getLong(Globals.ID_TAG, -1L);
+		DatabaseWrapper dbWrapper = new DatabaseWrapper(this);
+		dbWrapper.open();
+		workoutInstance.setWorkout((Workout) dbWrapper.getEntryById(id, Workout.class));
+		dbWrapper.close();
+
+		nameView.setText(workoutInstance.getWorkout().getName());		
+		
+		// Define a new adapter
+	    mAdapter = new ExerciseArrayAdapter(this, R.layout.exercises_list_row, workoutInstance.getWorkout().getExerciseList());
+
+	    // Assign the adapter to ListView
+	    ListView listView = (ListView) findViewById(R.id.exerciseListView);
+	    listView.setAdapter(mAdapter);
+
+	    // Define the listener interface
+	    OnItemClickListener mListener = new OnItemClickListener() {
+	    	
+	    		@Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	    						
+                        // Open dialog to edit this exercise
+                	Exercise e = workoutInstance.getWorkout().getExerciseList().get(position);
+                	DialogFragment fragment = AppDialogFragment.newInstance(e, false);
+        	        fragment.show(getFragmentManager(), getString(R.string.dialog_fragment_tag_do_exercise));
+                		
+                }
+	    };
+
+	    // Get the ListView and wired the listener
+	    listView.setOnItemClickListener(mListener);
+		
+    
+	}
+	
+	public void onDoExercise(Exercise e) {
+		// We want to add this exercise to the instance's list of exercises
+		workoutInstance.addExercise(e);
+		
+	}
+	
+	public void onFinishWorkout() {
+		
+		// We want to save the workout into the database as a workout instance
+		DatabaseWrapper dbWrapper = new DatabaseWrapper(this);
+		dbWrapper.open();
+		dbWrapper.createEntry(workoutInstance);
+		dbWrapper.close();
+		
+		finish(); // close the activity
+			
+	}
+}
---
+++ Checkpoint/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/gae/Uploader.java
@@ -0,0 +1,105 @@
+package cs65.dartmouth.get_swole.gae;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+
+import cs65.dartmouth.get_swole.Globals;
+import cs65.dartmouth.get_swole.MainActivity;
+import cs65.dartmouth.get_swole.classes.ProfileObject;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+public class Uploader {
+
+	Context mContext;
+	String mServerUrl;
+	
+	public Uploader (Context context, String serverURL) {
+		mContext = context;
+		mServerUrl = serverURL;
+	}
+	
+//	public boolean upload(ArrayList<ExerciseEntry> entryList) throws IOException {
+//		
+//		// Convert entries to string
+//		// Create new JSON array: 
+//		JSONArray jsonArray = new JSONArray();
+//		
+//		// Loop through the entries and turn to JSONObjects
+//		for (ExerciseEntry entry : entryList) {
+//			jsonArray.put(entry.toJSONObject());
+//		}
+//		
+//		// Convert the jsonArray to string
+//		String jsonArrayString = jsonArray.toString();
+//		
+//		// Get Shared Preferences
+//		SharedPreferences GCMPreferences = mContext.getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+//		
+//		// Get registration id:
+//		final String regId =  GCMPreferences.getString(MainActivity.PROPERTY_REG_ID, "");				
+//		
+//		if (!regId.isEmpty()) {
+//			
+//			Log.d("GCM", "regId is legit!!!");
+//			
+//			// Create new hashmap
+//			Map<String, String> hashmap = new HashMap<String, String>();
+//			
+//			// Put the regId with key “regId” into hashmap
+//			hashmap.put("regId", regId);
+//			
+//			// Put the data (the converted string jsonArray from earlier) with key “data” into hashmap
+//			hashmap.put("data", jsonArrayString);
+//	
+//			// Post
+//			ServerUtilities.post(mServerUrl, hashmap);
+//		}
+//		
+//		return true;
+//
+//	}
+//	
+	
+	public boolean uploadProfile(ProfileObject profile) throws IOException {
+		
+		Log.d(Globals.TAG, "uploadProfile");
+		
+		// Convert entries to string
+		// Create new JSON array: 
+		JSONArray jsonArray = new JSONArray();
+		
+		// Put profile into JSONArray
+		jsonArray.put(profile.toJSONObject());
+		
+		// Convert the jsonArray to string
+		String jsonArrayString = jsonArray.toString();
+		
+		// Get Shared Preferences
+		SharedPreferences GCMPreferences = mContext.getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+		
+		// Get registration id:
+		final String regId =  GCMPreferences.getString(MainActivity.PROPERTY_REG_ID, "");				
+		
+		if (!regId.isEmpty()) {
+			
+			// Create new hashmap
+			Map<String, String> hashmap = new HashMap<String, String>();
+			
+			// Put the regId with key “regId” into hashmap
+			hashmap.put("regId", regId);
+			
+			// Put the data (the converted string jsonArray from earlier) with key “data” into hashmap
+			hashmap.put("profile", jsonArrayString);
+	
+			// Post
+			ServerUtilities.post(mServerUrl, hashmap);
+		}
+	
+		return true;
+	}
+}
