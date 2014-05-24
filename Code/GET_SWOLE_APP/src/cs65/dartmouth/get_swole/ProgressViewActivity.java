f3dcddae1086005685514e1854c31f8a77d7bd8a
Patricia T. Neckowicz <Patricia T. Neckowicz@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-24 16:27:11 +0000

    Attempting to add progress graph

--- Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/ProgressViewActivity.java
+++ Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/ProgressViewActivity.java
@@ -11,6 +11,8 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.FrameLayout;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.LineGraphView;
 
 import cs65.dartmouth.get_swole.classes.Exercise;
@@ -22,7 +24,8 @@ public class ProgressViewActivity extends Activity {
 	boolean repsChecked;
 	boolean weightChecked;
 	Exercise exercise; // used to compare goals
-	ArrayList<WorkoutInstance> instances;
+	ArrayList<Exercise> instanceExercises;
+	ArrayList<WorkoutInstance> instanceWorkouts;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +41,26 @@ public class ProgressViewActivity extends Activity {
 		exercise = (Exercise) dbWrapper.getEntryById(eId, Exercise.class);
 		
 		// We need to get all workoutinstances of this workout 
-		instances = dbWrapper.getAllInstances(wId);
+		instanceWorkouts = dbWrapper.getAllInstances(wId);
 		dbWrapper.close();
 
+		// We need to grab the exercises from these instances
+		instanceExercises = new ArrayList<Exercise>();
+		for (WorkoutInstance wi : instanceWorkouts) {
+			
+			boolean added = false;
+			for (Exercise e : wi.getExerciseList()) {
+				if (e.getId() == eId) {
+					instanceExercises.add(e);
+					added = true;
+					break;
+				}			
+			}
+			if (!added) {
+				instanceExercises.add(null); // place holder so that instance exercises is the same size as instance workouts
+			}	
+		}
+		
 		// Create the graph
 		FrameLayout graphLayout = (FrameLayout) findViewById(R.id.graphLayout);
 		progressChart = new LineGraphView(this, exercise.getName());
@@ -72,8 +92,34 @@ public class ProgressViewActivity extends Activity {
 		
 	}
 	
+	// Based on checkbox preferences and exercise instances
 	public void refreshGraphView() {
-		
+		if (repsChecked) {
+			int dataIndex = 0;
+			GraphViewData [] repsData = new GraphViewData[instanceWorkouts.size()];
+			
+			for (int i = 0 ; i < instanceWorkouts.size(); i++) {
+				if (instanceExercises.get(i) != null) {
+					int maxReps = instanceExercises.get(i).getMaxReps();
+					if (maxReps != -1) repsData[dataIndex++] = new GraphViewData(instanceWorkouts.get(i).getTime().getTimeInMillis(), maxReps);
+				}
+			}
+			
+			progressChart.addSeries(new GraphViewSeries(repsData));
+		}
+		if (weightChecked) {
+			int dataIndex = 0;
+			GraphViewData [] weightData = new GraphViewData[instanceWorkouts.size()];
+			
+			for (int i = 0; i < instanceWorkouts.size(); i++) {
+				if (instanceExercises.get(i) != null) {
+					int maxWeight = instanceExercises.get(i).getMaxWeight();
+					if (maxWeight != -1) weightData[dataIndex++] = new GraphViewData(instanceWorkouts.get(i).getTime().getTimeInMillis(), maxWeight);
+				}
+			}
+			
+			progressChart.addSeries(new GraphViewSeries(weightData));
+		}
 	}
 
 	@Override
