2b27416882f067e310bf7341295bcb9c8104f6cd
Patricia T. Neckowicz <Patricia T. Neckowicz@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-24 15:58:52 +0000

    updated some files

--- GETSWOLE/Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/classes/ExerciseArrayAdapter.java
+++ GETSWOLE/Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/classes/ExerciseArrayAdapter.java
@@ -1,4 +1,4 @@
-package cs65.dartmouth.get_swole;
+package cs65.dartmouth.get_swole.classes;
 
 import java.util.List;
 
@@ -8,7 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import cs65.dartmouth.get_swole.classes.Exercise;
+import cs65.dartmouth.get_swole.R;
+import cs65.dartmouth.get_swole.R.id;
+import cs65.dartmouth.get_swole.R.layout;
 
 /**
  * 
