d10da90290dbcb5e89e7f869cc478271a5d0b679
Cameron J. Price <Cameron J. Price@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-25 03:51:33 +0000

    update all versions of code

--- Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/gae/Uploader.java
+++ Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/gae/Uploader.java
@@ -1,6 +1,7 @@
 package cs65.dartmouth.get_swole.gae;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,6 +10,7 @@ import org.json.JSONArray;
 import cs65.dartmouth.get_swole.Globals;
 import cs65.dartmouth.get_swole.MainActivity;
 import cs65.dartmouth.get_swole.classes.ProfileObject;
+import cs65.dartmouth.get_swole.classes.Workout;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
@@ -22,48 +24,6 @@ public class Uploader {
 		mContext = context;
 		mServerUrl = serverURL;
 	}
-	
-//	public boolean upload(ArrayList<ExerciseEntry> entryList) throws IOException {
-//		
-//		// Convert entries to string
-//		// Create new JSON array: 
-//		JSONArray jsonArray = new JSONArray();
-//		
-//		// Loop through the entries and turn to JSONObjects
-//		for (ExerciseEntry entry : entryList) {
-//			jsonArray.put(entry.toJSONObject());
-//		}
-//		
-//		// Convert the jsonArray to string
-//		String jsonArrayString = jsonArray.toString();
-//		
-//		// Get Shared Preferences
-//		SharedPreferences GCMPreferences = mContext.getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
-//		
-//		// Get registration id:
-//		final String regId =  GCMPreferences.getString(MainActivity.PROPERTY_REG_ID, "");				
-//		
-//		if (!regId.isEmpty()) {
-//			
-//			Log.d("GCM", "regId is legit!!!");
-//			
-//			// Create new hashmap
-//			Map<String, String> hashmap = new HashMap<String, String>();
-//			
-//			// Put the regId with key “regId” into hashmap
-//			hashmap.put("regId", regId);
-//			
-//			// Put the data (the converted string jsonArray from earlier) with key “data” into hashmap
-//			hashmap.put("data", jsonArrayString);
-//	
-//			// Post
-//			ServerUtilities.post(mServerUrl, hashmap);
-//		}
-//		
-//		return true;
-//
-//	}
-//	
 	
 	public boolean uploadProfile(ProfileObject profile) throws IOException {
 		
@@ -102,4 +62,44 @@ public class Uploader {
 	
 		return true;
 	}
+	
+	public boolean uploadWorkouts(ArrayList<Workout> workouts) throws IOException {
+		
+		// Convert entries to string
+		// Create new JSON array: 
+		JSONArray jsonArray = new JSONArray();
+		
+		// Loop through the entries and turn to JSONObjects
+		for (Workout workout : workouts) {
+			jsonArray.put(workout.toJSONObject()); // NEED TO DO THIS!!!
+		}
+		
+		// Convert the jsonArray to string
+		String jsonArrayString = jsonArray.toString();
+		
+		// Get Shared Preferences
+		SharedPreferences GCMPreferences = mContext.getSharedPreferences(MainActivity.class.getSimpleName(), Context.MODE_PRIVATE);
+		
+		// Get registration id:
+		final String regId =  GCMPreferences.getString(MainActivity.PROPERTY_REG_ID, "");				
+		
+		if (!regId.isEmpty()) {
+			
+			// Create new hashmap
+			Map<String, String> hashmap = new HashMap<String, String>();
+			
+			// Put the regId with key “regId” into hashmap
+			hashmap.put("regId", regId);
+			
+			// Put the data (the converted string jsonArray from earlier) with key “data” into hashmap
+			hashmap.put("workouts", jsonArrayString);
+	
+			// Post
+			ServerUtilities.post(mServerUrl, hashmap);
+		}
+		
+		return true;
+
+	}
+	
 }
