5f899ad9386715d77083a334d70f8c9bef76cc74
Cameron J. Price <Cameron J. Price@b7a29500-2b41-464b-9cf8-02187402cde8>
2014-05-27 21:11:07 +0000

    update settings and profile

--- Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/SettingsActivity.java
+++ Code/GET_SWOLE_APP/src/cs65/dartmouth/get_swole/SettingsActivity.java
@@ -1,7 +1,15 @@
 package cs65.dartmouth.get_swole;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.widget.Toast;
 
 
 /**
@@ -25,6 +33,31 @@ public class SettingsActivity extends PreferenceActivity {
 		
 	    // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.settings);
+        
+        Preference workoutPrefs = (Preference) findPreference("deleteWorkouts");
+        workoutPrefs.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+        	public boolean onPreferenceClick(Preference preference) {
+        		Toast.makeText(getBaseContext(), "Workouts", Toast.LENGTH_SHORT).show();
+        		return true;
+        	}
+        });
+        
+        Preference historyPrefs = (Preference) findPreference("deleteHistory");
+        historyPrefs.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+        	public boolean onPreferenceClick(Preference preference) {
+        		Toast.makeText(getBaseContext(), "History", Toast.LENGTH_SHORT).show();
+        		return true;
+        	}
+        });
+        
+        Preference deletePrefs = (Preference) findPreference("deleteAll");
+        deletePrefs.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+        	public boolean onPreferenceClick(Preference preference) {
+        		Toast.makeText(getBaseContext(), "All", Toast.LENGTH_SHORT).show();
+        		return true;
+        	}
+        });
+        
 	}
 
 }
